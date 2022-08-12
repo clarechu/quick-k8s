4eb4550448d42d223a3df850deb6a229f06d15b1
clarechu <1062186165@qq.com>
2022-08-12 11:06:50 +0800

    修复安装kubelet不成功的问题

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 
 GOOS=linux GOHOSTARCH=amd64 go build -o quickctl
 
-tar -cvf quickctl-v0.0.1-linux-amd64.tar.gz quickctl
+tar -cvf quickctl-v0.0.2-linux-amd64.tar.gz quickctl
+
+rm -rf quickctl
--- quick.sh
+++ quick.sh
@@ -9,7 +9,7 @@ rm -rf $basic_path
 git clone https://github.com/clarechu/quick-k8s.git $basic_path
 
 
-wget https://github.com/clarechu/quick-k8s/releases/download/v0.0.1/quickctl-v0.0.1-linux-amd64.tar.gz -O $basic_path/quickctl-v0.0.1-linux-amd64.tar.gz
+wget https://github.com/clarechu/quick-k8s/releases/download/v0.0.2/quickctl-v0.0.2-linux-amd64.tar.gz -O $basic_path/quickctl-v0.0.1-linux-amd64.tar.gz
 
 tar -xvf $basic_path/quickctl-v0.0.1-linux-amd64.tar.gz -C $basic_path \
   && rm -rf $basic_path/quickctl-v0.0.1-linux-amd64.tar.gz
